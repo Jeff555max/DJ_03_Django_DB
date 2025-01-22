cd80394c669c280fb104295c3380dd1a92617172
Jeff555max <vadik98765@gmail.com>
2025-01-22 18:43:00 +0500

    1

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
+    path('new/', views.new ),
+    path('new2/', views.new2 ),
+    path('new3/', views.new3 ),
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def new(request):
+    return HttpResponse('<h1> Первая страница </h1>')
+
+def new2(request):
+    return HttpResponse('<h1> Вторая страница </h1>')
+
+def new3(request):
+    return HttpResponse('<h1> Третья страница </h1>')
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html') # файлы HTML пока не созданы
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html') # файлы HTML пока не созданы
 
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'services.html') # файлы HTML пока не созданы
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact.html') # файлы HTML пока не созданы
