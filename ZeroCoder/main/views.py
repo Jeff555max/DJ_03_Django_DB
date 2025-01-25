6439b87fcfa30ac4dbde771a703b28a70d26f009
Jeff555max <vadik98765@gmail.com>
2025-01-25 14:59:47 +0500

    Update code 1

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
 def new(request):
     return HttpResponse('<h1> Первая страница </h1>')
 
@@ -9,15 +22,3 @@ def new2(request):
 
 def new3(request):
     return HttpResponse('<h1> Третья страница </h1>')
-
-def index(request):
-    return render(request, 'index.html') # файлы HTML пока не созданы
-
-def about(request):
-    return render(request, 'about.html') # файлы HTML пока не созданы
-
-def services(request):
-    return render(request, 'services.html') # файлы HTML пока не созданы
-
-def contact(request):
-    return render(request, 'contact.html') # файлы HTML пока не созданы
