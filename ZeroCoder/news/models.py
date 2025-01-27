b3047cbeb88166ff2dc3e8413764b2d36463d27c
Jeff555max <vadik98765@gmail.com>
2025-01-27 18:37:24 +0500

    3

--- ZeroCoder/news/models.py
+++ ZeroCoder/news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class NewsPost(models.Model):
+    title = models.CharField('Название новости', max_length=50) # Класс CharField помогает создавать поля, в которых можно записать не более 250 символов.
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Какие то новости')
+    pub_date = models.DateTimeField('Дата публикации')
