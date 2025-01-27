b8c8a548fdc04f323ee14f8c79c577f262c65b2c
Jeff555max <vadik98765@gmail.com>
2025-01-27 21:07:09 +0500

    3

--- ZeroCoder/news/admin.py
+++ ZeroCoder/news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import NewsPost # точка не обязательна, она говорит о том что мы берем models из той же
+                             # директории где находимся
 
 # Register your models here.
+
+# Регистрируем новую таблицу внутри нашего проекта
+
+admin.site.register(NewsPost)
