c63884624297f4c2048ba5370a6a0f2f75418871
Jeff555max <vadik98765@gmail.com>
2025-01-29 23:30:32 +0500

    4

--- ZeroCoder/news/forms.py
+++ ZeroCoder/news/forms.py
@@ -1,16 +1,17 @@
-from django import forms
 from .models import NewsPost
+from django.forms import ModelForm, TextInput,Textarea, DateTimeInput
 
-class NewsPostForm(forms.ModelForm):
+
+class NewsPostForm(ModelForm):
     class Meta:
         model = NewsPost
         fields = ['title', 'short_description', 'text', 'pub_date',]
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите заголовок новости'}),
-            'short_description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
-            'text': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости', 'rows': 4}),
-            'pub_date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации', 'type': 'date'}),
-            'writers': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите Ваше имя'}),
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите заголовок новости'}),
+            'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
+            'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости', 'rows': 4}),
+            'pub_date':DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации', 'type': 'date'}),
+            'writers': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите Ваше имя'}),
         }
 # Добавляем атрибуты к конструкциям, чтобы именно они приняли стили Bootstrap, а не теги.
 # Чтобы всё задекорировать, заходим в forms.py. Создаём специальный виджет, в котором будем хранить словарь
--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import NewsPost
 from .forms import NewsPostForm
-from django.shortcuts import redirect
 
-
-# Create your views here.
 
 def news_home(request):
     news = NewsPost.objects.all()
@@ -14,13 +11,13 @@ def news_home(request):
 def create_news(request):
     error = ""
     if request.method == 'POST':
-        form = NewsPostForm(request.POST)  # Сюда сохранится информация от пользователя, которую он ввел в форму.
+        form = NewsPostForm(request.POST)
         if form.is_valid():
             form.save()
-
+            return redirect('news_home')  # Убедитесь, что в urls.py есть path с name='news_home'
         else:
             error = "Данные были заполнены некорректно"
     else:
-        form = NewsPostForm()  # Форма создаётся только при GET-запросе
+        form = NewsPostForm()  # Форма создаётся при GET-запросе
 
     return render(request, 'news/add_new_post.html', {'form': form, 'error': error})
