c5e89aa70b9bfb51e4dccc43982107f53b1958db
Jeff555max <vadik98765@gmail.com>
2025-01-29 23:00:22 +0500

    4

--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from .models import NewsPost
 from .forms import NewsPostForm
+from django.shortcuts import redirect
+
 
 # Create your views here.
 
 def news_home(request):
-	news = NewsPost.objects.all()
-	return render(request, 'news/super_news.html',{'news': news})
+    news = NewsPost.objects.all()
+    return render(request, 'news/super_news.html', {'news': news})
+
 
 def create_news(request):
-	form = NewsPostForm()
-	return render(request, 'news/add_new_post.html',{'form': form} )
+    error = ""
+    if request.method == 'POST':
+        form = NewsPostForm(request.POST)  # Сюда сохранится информация от пользователя.
+        if form.is_valid():
+            form.save()
 
+        else:
+            error = "Данные были заполнены некорректно"
+    else:
+        form = NewsPostForm()  # Форма создаётся только при GET-запросе
 
-
+    return render(request, 'news/add_new_post.html', {'form': form, 'error': error})
