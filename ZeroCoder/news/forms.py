7028b7607491ea53a01d15795cf3326d03233849
Jeff555max <vadik98765@gmail.com>
2025-01-29 22:28:52 +0500

    3

--- ZeroCoder/news/forms.py
+++ ZeroCoder/news/forms.py
@@ -1,18 +1,16 @@
+from django import forms
 from .models import NewsPost
-from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
-
-# Форму нужно связать с моделью, с базой данных
-class NewsPostForm(ModelForm):
+class NewsPostForm(forms.ModelForm):
     class Meta:
         model = NewsPost
-        fields = ['title', 'short_description', 'text', 'pub_date']  # поля берутся из файла news.models
-
-    widgets = {
-        'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости'}),
-        'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
-        'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости'}),
-        'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации'})
-    }
+        fields = ['title', 'short_description', 'text', 'pub_date',]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите заголовок новости'}),
+            'short_description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
+            'text': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости', 'rows': 4}),
+            'pub_date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации', 'type': 'date'}),
+            'writers': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите Ваше имя'}),
+        }
 # Добавляем атрибуты к конструкциям, чтобы именно они приняли стили Bootstrap, а не теги.
-# Чтобы всё задекорировать, заходим в forms.py. Создаём специальный виджет, в котором будем хранить словарь.
+# Чтобы всё задекорировать, заходим в forms.py. Создаём специальный виджет, в котором будем хранить словарь
